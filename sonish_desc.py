c2fd748df5699bc582dbe46a87da4883d9e1e4e3
Sonish khanal <sonishkhanal@gmail.com>
2024-09-13 12:29:06 +0930

    Update sonish_desc.py

--- sonish_desc.py
+++ sonish_desc.py
@@ -1,26 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Loading the CSV files
-df1 = pd.read_csv('C:/Users/singh/OneDrive/Desktop/data science code/dataset1.csv')
-df3 = pd.read_csv('C:/Users/singh/OneDrive/Desktop/data science code/dataset2.csv')
-df2 = pd.read_csv('C:/Users/singh/OneDrive/Desktop/data science code/dataset3.csv')
-pd.set_option('display.max_columns', None)
-
-# Merging datasets on 'ID'
-merged_df = pd.merge(df1, df2, on='ID', how="inner")
-final_df = pd.merge(merged_df, df3, on="ID", how="inner")
-
-# Dropping duplicates
-final_df = final_df.drop_duplicates()
-
-# Defining column indices for numerical and ordinal columns
-numerical_column_indices = [4, 5, 6, 7, 8, 9, 10, 11]  # Replace with actual indices for numerical data
-ordinal_column_indices = list(range(11, 26))  # Replace with actual indices for ordinal data
-
-numerical_columns = final_df.iloc[:, numerical_column_indices]
-ordinal_columns = final_df.iloc[:, ordinal_column_indices]
-
 # 1. Box plots for numerical columns by gender
 import pandas as pd
 import matplotlib.pyplot as plt
